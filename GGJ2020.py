0b8581087d86b282f6d75d76ff92af84b58c2aaf
spirkelmann <simon.pirkelmann@gmail.com>
2020-02-01 21:21:36 +0100

    added temperature logic

--- GGJ2020.py
+++ GGJ2020.py
@@ -52,8 +52,6 @@ class GameManager:
         self.map = Map(self.grid_vertical, self.grid_horizontal)
 
         self.bees = {
-            0: Bee((3,3), id=0, color=(255,0,0)) ,
-            1: Bee((5,1), id=1, color=(0,255,0))
         }
 
         self.queue = queue.Queue(maxsize=10)
@@ -73,6 +71,7 @@ class GameManager:
         self.flowers = []
         self.flowers_collected = 0
         self.intruders = []
+        self.temperature = 50
 
     def new_color(self):
         func = (random.randint(0,255) for i in range(3))
@@ -155,7 +154,7 @@ class GameManager:
         try:
             pos = self.bees[id].new_pos(direction)
             if self.hive.is_valid(pos):
-                print('pos:', pos[0], pos[1])
+                self.temperature += 2/len(self.bees.keys())
                 self.bees[id].move_bee(pos)
         except:
             pass
@@ -171,6 +170,16 @@ class GameManager:
             surface = self.screen
         for intruder in self.intruders:
             intruder.paint(surface)
+
+    def apply_temperature(self):
+        self.temperature -= 0.1
+
+    def draw_temperature(self, surface=None):
+        if surface is None:
+            surface = self.screen
+        width = int(self.disp_width * 0.02)
+        height = max(0,int(self.temperature))
+        pygame.draw.rect(surface, (255,0,0), ((int(self.disp_width * 0.9),int(self.disp_height * 0.5)-height), (width, height)) )
 
 # define a main function
 def main():
@@ -220,6 +229,7 @@ def main():
         game.handle_input()
         game.handle_bot_queue()
         game.collect_flowers()
+        game.apply_temperature()
 
         game.animate_bees()
         game.hive.draw_grid(game.screen)
@@ -227,6 +237,7 @@ def main():
 
         game.draw_flowers()
         game.draw_intruders()
+        game.draw_temperature()
 
         pygame.display.flip()
         # draw a line
