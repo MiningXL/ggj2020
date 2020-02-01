a4644f9eb2bbceb696a545371b0737d996c4f853
Patrick Huppmann <miningxl@gmail.com>
2020-02-01 01:26:33 +0100

    Control Site

---
+++ html/js/controller.js
@@ -0,0 +1,21 @@
+function butt_top_left_click() {
+    alert("Top Left");
+}
+function butt_top_click() {
+    alert("Top");
+}
+function butt_top_right_click() {
+    alert("Top Right");
+}
+function butt_middle_click() {
+    alert("Middle");
+}
+function butt_bottom_left_click() {
+    alert("Bottom Left");
+}
+function butt_bottom_click() {
+    alert("Bottom");
+}
+function butt_bottom_right_click() {
+    alert("Bottom Right");
+}
