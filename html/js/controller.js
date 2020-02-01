4014cc2a1c23e0a85c109ad3557c6ba6831878a7
Patrick Huppmann <miningxl@gmail.com>
2020-02-01 14:59:49 +0100

    now posts directions with click

--- html/js/controller.js
+++ html/js/controller.js
@@ -1,23 +1,30 @@
+function post_to_server(dir) {
+    var xhttp = new XMLHttpRequest();
+    var url = "http://localhost:8080";
+    var id = "0";
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("var1=" + id + "&var2=" + dir);
+}
+
 function butt_top_left_click() {
-    alert("Top Left");
+    post_to_server("tl");
 }
 function butt_top_click() {
-    alert("Top");
+    post_to_server("t");
 }
 function butt_top_right_click() {
-    alert("Top Right");
+    post_to_server("tr");
 }
-
 function butt_middle_click() {
-    alert("Middle");
+    post_to_server("m");
 }
-
 function butt_bottom_left_click() {
-    alert("Bottom Left");
+    post_to_server("bl");
 }
 function butt_bottom_click() {
-    alert("Bottom");
+    post_to_server("b");
 }
 function butt_bottom_right_click() {
-    alert("Bottom Right");
+    post_to_server("br");
 }
